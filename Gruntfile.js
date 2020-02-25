e12585da9ff5c1d6c28022fb7c26a6b0ad023980
xiaoxuqin <2837303093@qq.com>
2020-02-25 17:33:43 +0800

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt){
+    // 插件的具体配置信息
+    grunt.initConfig({
+        less:{
+            complie: {
+                files: {'dist/complied.css':'css/*.less'}
+            }
+        }
+    });
+
+    // 加载插件
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    // 构建的任务清单
+    grunt.registerTask('default', ['less:complie']);
+}
