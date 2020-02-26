efdd67d29beabe9f7d979219eb79d1ffafaabaf4
秦晓旭 <33643037+xiaoxuqin@users.noreply.github.com>
2020-02-26 12:42:25 +0800

    Update rectangle.js

--- rectangle.js
+++ rectangle.js
@@ -1,37 +1,21 @@
-$(function (){
-  //get dom elem
-  var $width  = $('#width'),
-      $heigth = $('#heigth'),
-      $btn    = $('#calculate'),
-      $per = $('#perimeter'),
-      $area = $('#area');
-  /**
-  * decimalSave
-  * 小数点后面保留第n位
-  *
-  * @param num
-  * @param n
-  * @returns {undefined}
-  */
-  function decimalSave(num,n){
-    return Math.round(num * Math.pow(10,n)) / Math.pow(10,n);
-  }
-  /*calc button click event*/
-  $btn.click(function (){
-    //get value
-    var w = Number($width.val());
-    var h = Number($heigth.val());
-    //calculate
-    var p = 2*(w+h);
-    var a = w*h;
-    //decimalSave
-    //console.log(decimalSave(p,2));
-    p = decimalSave(p,2);
-    a = decimalSave(a,2);
-    //output
-    $per.val(p);
-    $area.val(a);
-  }) 
+/* global $: true */
+$(function(){
+    var $width = $('#width'),
+        $heigth = $('#heigth'),
+        $btn = $('#calculate'),
+        $per = $('#perimeter'),
+        $area = $('#area');
+    function decimalSave(num, n){
+        return Math.round(num*Math.pow(10, n))/Math.pow(10,n);
+    }
+    $btn.click(function(){
+        var w = Number($width.val());
+        var h = Number($heigth.val());
+        var p = 2*(w+h);
+        var a = w*h;
+        p = decimalSave(p, 2);
+        a = decimalSave(a, 2);
+        $per.val(p);
+        $area.val(a);
+    });
 });
-
-
