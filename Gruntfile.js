e28ab9db42661dcd251fae16e637409d454a7f50
xiaoxuqin <2837303093@qq.com>
2020-03-03 17:30:39 +0800

    uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        cssmin:{
-            'rectangle.min.css':'rectangle.css'
+        uglify:{
+            'rectangle.min.js':'rectangle.js'
         }
     });
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.registerTask('default', ['cssmin']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
 }
